f127ea61c05f2107948fad5b026b9ecd5a89f2ad
crlse42 <carlosdlt42@hotmail.com>
2023-07-02 23:55:44 -0700

    Added JavaScript to Prework Project

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,29 @@
-var topics = ["HTML", "CSS","Git","JavaScript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[(Math.random()* topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === "HTML") {
-    console.log("Let's Study HTML!");
-} else if (topic === "CSS") {
-    console.log("Let's Study CSS!");
-} else if (topic === "JavaScript") {
-    console.log("Let's Study JavaScript!");
-} else {
-    console.log("Please Try Again!")
+function selectTopics() {
+    if (randomTopic === "HTML") {
+        console.log("Let's Study HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's Study CSS!");
+    } else if (randomTopic === "JavaScript") {
+        console.log("Let's Study JavaScript!");
+    } else {
+        console.log("Please Try Again!");
+    }
 }
-*/
+
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study First?");
+selectTopics();
+
 
 
 
@@ -26,15 +35,15 @@ if (topic === "HTML") {
 // var shapes = ["triangle", "square", "pentagon", "circle"];
 // // console.log(shapes[0]);
 
-// // //A for loop contains the following three important statements: The first statement determines the starting point for our loop. 
+// // //A for loop contains the following three important statements: The first statement determines the starting point for our loop.
 // // //Since we want to start at the beginning of the array, we declare a variable x and give it a value of 0 (because the first index of an array is always 0), as shown in the following line of code:
 // // //var x = 0;
 
-// // //The second statement is the condition. As long as the number in the variable x is less than the length of the array, the loop will keep running. 
+// // //The second statement is the condition. As long as the number in the variable x is less than the length of the array, the loop will keep running.
 // // //This will allow the loop to execute one time for each item in the array. We use the length built-in array property on our shapes array to express that condition, as shown in the following line of code:
 // // x < shapes.length;
 
-// // //•	The final statement is what allows the array to iterate over each item. Since indices follow a predictable pattern, we know the number increases by 1 each time. 
+// // //•	The final statement is what allows the array to iterate over each item. Since indices follow a predictable pattern, we know the number increases by 1 each time.
 // // //The code x++ is a shortcut way of writing x+1. Now every time the code loops, x will increase by 1, as shown in the following line of code:
 // // x++
 
